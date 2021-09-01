26c4ff5ba0f60f4203896506ded4169ae059c723
KirillEmets <kirill.yemets@gmail.com>
2021-09-01 14:46:20 +0300

    Update user in authStateListener and sign in Anonymously if user is null

--- app/src/main/java/com/kirillemets/kirillyemetsnotes/model/network/auth/AuthViewModel.kt
+++ app/src/main/java/com/kirillemets/kirillyemetsnotes/model/network/auth/AuthViewModel.kt
@@ -1,42 +1,40 @@
 package com.kirillemets.kirillyemetsnotes.model.network.auth
 
-import android.app.Activity.RESULT_OK
 import androidx.lifecycle.ViewModel
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.transform
+import kotlinx.coroutines.flow.StateFlow
 
 class AuthViewModel : ViewModel() {
-    private val firebaseUser: MutableStateFlow<FirebaseUser?> = MutableStateFlow(null)
-    var user = firebaseUser.transform { fbUser ->
-        emit(
-            fbUser?.let {
-                User(
-                    name = fbUser.displayName ?: "",
-                    uid = fbUser.uid
-                )
-            }
-        )
-    }
-    val onSignIn: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    private val _user: MutableStateFlow<User?> = MutableStateFlow(null)
+    var user: StateFlow<User?> = _user
 
     private val auth = Firebase.auth
 
-    init {
-        auth.addAuthStateListener { firebaseUser.value = it.currentUser }
+    private val authStateListener = FirebaseAuth.AuthStateListener {
+        it.currentUser?.let { firebaseUser ->
+            _user.value = User(
+                name = firebaseUser.displayName ?: "",
+                uid = firebaseUser.uid,
+                isAnon = firebaseUser.isAnonymous
+            )
+        } ?: run {
+            auth.signInAnonymously()
+        }
     }
 
-    fun onSignIn(result: FirebaseAuthUIAuthenticationResult) {
-        if(result.resultCode == RESULT_OK) {
-            onSignIn.value = true
-        }
 
+    init {
+        auth.addAuthStateListener(authStateListener)
     }
 
     fun signOut() {
         auth.signOut()
+    }
+
+    override fun onCleared() {
+        auth.removeAuthStateListener(authStateListener)
     }
 }
